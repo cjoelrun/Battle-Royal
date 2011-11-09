4c737a053f98d95a9a61541020bb0dfc5683f638
Cameron Lopez <cameronjlopez@gmail.com>
2011-11-08 18:01:22 -0600

    Resized markes, added print layout.

---
+++ Content/markers/auto/layout.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook "layout"
+ (lambda ()
+    (TeX-run-style-hooks
+     "fullpage"
+     "graphicx"
+     "latex2e"
+     "art10"
+     "article"
+     "10pt"
+     "letterpaper")))
+
